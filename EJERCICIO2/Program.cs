5ec647107150238bf91532a11c40a2f73917c4c4
Pablo Durán <duranpablo.alejandro@gmail.com>
2023-04-21 16:14:03 -0300

    parte del ejercicio 2

--- EJERCICIO2/Program.cs
+++ EJERCICIO2/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("PRÓXIMAMENTE");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4Enum
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+
+        {
+            string v = "si";
+            while (v == "si")
+            {
+
+                string L = "lunes";
+                string m = "Martes";
+
+                string s = "sabado";
+
+                Console.WriteLine("ingrese un dia de semana");
+                string dia = Console.ReadLine();
+
+                if ((dia == L) || (dia == m)) { Console.WriteLine("dia de semana"); }
+                else
+                {
+                    Console.WriteLine("finde");
+                }
+                Console.WriteLine("quiere seguir?");
+                v = Console.ReadLine();
+            }
+            Console.WriteLine("programa terminado");
+
+        }
+    }
+}  
+
